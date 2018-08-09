7c26bc1869ebda4128d13fa69bd22c7f56929c15
Thabiso Mohatlane <thabisomoha@gmail.com>
2018-08-09 11:11:22 +0200

    added folders for static files

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 import express from 'express';
-import data from './data/data.json'
+import data from './data/data.json';
 
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.use('./images', express.static('images'));
+
+app.get('/', (req, res) => {
+  res.json(data);
+});
+
+app.post('/newItem', (req, res) => {
+  res.send(`a post request with /newItem route on port ${PORT}`);
+});
+
+app.put('/item', (req, res) => {
+  res.send(`a put request with /item route on port ${PORT}`);
+});
+
+app.delete('/item', (req, res) => {
+  res.send(`a delete request with /item route on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Your server is running on http://localhost:${PORT}`);
