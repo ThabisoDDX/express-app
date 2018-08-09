f9163e885491053bbd548b393579f95ea1c7a1b8
Thabiso Mohatlane <thabisomoha@gmail.com>
2018-08-09 14:02:18 +0200

    changes in index.js and .editorconfig

--- index.js
+++ index.js
@@ -5,27 +5,39 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
-app.use('./images', express.static('images'));
+app.use('/images', express.static('images'));
 
 app.get('/', (req, res) => {
   res.json(data);
 });
 
-app.post('/newItem', (req, res) => {
-  res.send(`a post request with /newItem route on port ${PORT}`);
-});
+app.get('/item/:id', (req, res, next) => {
+  console.log(req.params.id);
+  let user = Number(req.params.id);
+  console.log(user);
+  console.log(data[user]);
+  res.send(data[user]);
+  next();
+}, (req, res) =>
+console.log('Did you get the right data?')
+);
 
-app.put('/item', (req, res) => {
-  res.send(`a put request with /item route on port ${PORT}`);
-});
 
-app.delete('/item', (req, res) => {
-  res.send(`a delete request with /item route on port ${PORT}`);
-});
+app.route('/item')
+  .get((req, res) =>
+    //res.download('images/rocket.jpg')
+    //res.redirect('https://www.linkedin.com')
+    //res.end();
+    res.send(`a get request with /item route on port ${PORT}`)
+  ).put((req, res) =>
+    res.send(`a put request with /newItem route on port ${PORT}`)
+  ).delete((req, res) =>
+    res.send(`a delete request with /item route on port ${PORT}`)
+  );
 
 app.listen(PORT, () => {
-  console.log(`Your server is running on http://localhost:${PORT}`);
-  console.log(data);
-}
+    console.log(`Your server is running on http://localhost:${PORT}`);
+    console.log(data);
+  }
 );
 
